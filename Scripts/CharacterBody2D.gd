bbdb680e1944c093b2eaa8c9fdf9b61cfd1ec885
Sigmha <arthur.chanlock@gmail.com>
2024-08-15 16:41:26 +0200

    Ajout player et attaque simple pour test

--- Scripts/CharacterBody2D.gd
+++ Scripts/CharacterBody2D.gd
@@ -1,14 +1,31 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var init_weapon_position:Vector2i
+var init_collision_player_position:Vector2
+var looking_direction: int = 1
 
+@onready var player_sprite = $PlayerSprite
+@onready var weapon = $Weapon 
+@onready var player_collision = $PlayerCollision
+
+func _ready():
+	init_weapon_position = weapon.position
+	init_collision_player_position = player_collision.position
 
 func _physics_process(delta):
+	
+	move_player(delta)
+	
+	attack()
+
+	move_and_slide()
+
+func move_player(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -19,10 +36,41 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		change_side(direction)
+		looking_direction = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func change_side(direction):
+	if direction == 1:
+		player_sprite.flip_h = false
+		weapon.rotation = 0
+		player_collision.position = init_collision_player_position
+		weapon.position.y = init_weapon_position.y
+		
+	elif direction == -1:
+		player_sprite.flip_h = true
+		weapon.rotation = PI
+		weapon.position.y = init_weapon_position.y - 1
+		player_collision.position = init_collision_player_position * Vector2(-1, 1)
+		
+
+func attack():
+	var idle_length = 5
+	var attack_length = 11
+	
+	if Input.is_action_pressed("LeftClick"):
+		player_sprite.set_frame(1)
+	else:
+		player_sprite.set_frame(0)
+	
+	
+	if player_sprite.get_frame() == 0:
+		weapon.position.x = looking_direction * idle_length
+	else:
+		weapon.position.x = looking_direction * attack_length
+	
---
+++ Scripts/main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	close_game()
+
+func close_game():
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
