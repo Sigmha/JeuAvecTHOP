5e9a301ea58dc7cab0dbf94b5f41eff693d3028f
THOpegasus <thopegaus@gmail.com>
2024-08-23 01:21:00 +0200

    debut intro

---
+++ Scenes/Camera2D_monde_principale.gd
@@ -0,0 +1,23 @@
+extends Camera2D
+
+
+@onready var player = $"../Player"
+var y_en_plus : int = -450
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position = postition_joueur(delta)
+
+
+func postition_joueur(delta):
+	var x = lerp(position,player.position, 5*delta)
+	var pos = Vector2(x[0],y_en_plus)
+	print(pos)
+	return pos
--- Scenes/monde_principale.gd
+++ Scenes/monde_principale.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+@onready var animation_player = $AnimationPlayer
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	animation_player.play("intro")
 
 
 func _process(_delta):
